a67572fbf3bfe4a5d59c7de7ae825d1071ec512c
Umut Şimşek <umutsimsek96@gmail.com>
2019-11-12 02:14:55 +0300

    Kitap kontrol paneli yazıldı.

--- dBook/Controllers/AdminController.cs
+++ dBook/Controllers/AdminController.cs
@@ -1,22 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using dBook.Models;
 namespace dBook.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    //[Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
-        // GET: Admin
+        dBookContext db = new dBookContext();
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult BooksControl()
         {
+            var books = db.Books.ToList();
+            return View(books);
+        }
+        public ActionResult CreateBook()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CreateBook(Books book,HttpPostedFileBase file)
+        {
+            if(file != null && file.ContentLength > 0)
+            {
+                var path = Path.Combine(Server.MapPath("~/img/BookPhoto"), file.FileName);
+                file.SaveAs(path);
+                book.BOOK_PHOTO = path;
+            }
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return View();
+        }
+        public ActionResult EditBook(int id)
+        {
+            var book = db.Books.Find(id);
+            return View(book);
+        }
+        [HttpPost]
+        public ActionResult EditBook([Bind(Include ="BOOK_ID,BOOK_NAME,BOOK_DESCRIPTION,BOOK_PHOTO")]Books book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult DeleteBook(int id)
+        {
+            Books book = db.Books.Find(id);
+            if(book == null)
+            {
+                return HttpNotFound();
+            }
+            db.Books.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
         public ActionResult AuthorsControl()
         {
