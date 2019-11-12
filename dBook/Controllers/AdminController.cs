2670e264e10650ee13d61eda7abc419f4bee54d5
Umut Şimşek <umutsimsek96@gmail.com>
2019-11-12 11:44:32 +0300

    Yazar kontrol paneli eklendi.

--- dBook/Controllers/AdminController.cs
+++ dBook/Controllers/AdminController.cs
@@ -23,12 +23,19 @@ namespace dBook.Controllers
         }
         public ActionResult CreateBook()
         {
+            var authors = db.Authors.ToList();
+            ViewBag.authors = new SelectList(authors,"AUTHOR_ID","AUTHOR_NAME");
             return View();
         }
         [HttpPost]
         public ActionResult CreateBook(Books book,HttpPostedFileBase file)
         {
-            if(file != null && file.ContentLength > 0)
+            var authors = db.Authors.ToList();
+            ViewBag.authors = new SelectList(authors, "AUTHOR_ID", "AUTHOR_NAME");
+
+            var author = db.Authors.Find(book.AUTHOR.AUTHOR_ID);
+            book.AUTHOR = author;
+            if (file != null && file.ContentLength > 0)
             {
                 var path = Path.Combine(Server.MapPath("~/img/BookPhoto"), file.FileName);
                 file.SaveAs(path);
@@ -68,7 +75,46 @@ namespace dBook.Controllers
         }
         public ActionResult AuthorsControl()
         {
+            var authors = db.Authors.ToList();
+            return View(authors);
+        }
+        public ActionResult CreateAuthor()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CreateAuthor(Authors author)
+        {
+            db.Authors.Add(author);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult EditAuthor(int id)
+        {
+            Authors author = db.Authors.Find(id);
+            return View(author);
+        }
+        [HttpPost]
+        public ActionResult EditAuthor([Bind(Include = "AUTHOR_ID,AUTHOR_NAME,AUTHOR_LASTNAME,AUTHOR_DESCRIPTION")]Authors author)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(author).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult DeleteAuthor(int id)
+        {
+            var author = db.Authors.Find(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            db.Authors.Remove(author);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
         public ActionResult UsersControl()
         {
