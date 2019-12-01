4f93bfe5d6ec9c72afd0f8b3ba0d2e27a6f0747e
Umut Şimşek <umutsimsek96@gmail.com>
2019-12-01 22:05:45 +0300

    UserViewModel oluşturuldu.

--- dBook/Controllers/UserController.cs
+++ dBook/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using dBook.Models;
+using dBook.ViewModels;
 namespace dBook.Controllers
 {
     public class UserController : Controller
@@ -87,16 +88,20 @@ namespace dBook.Controllers
 
             return View();
         }
-        //[Authorize]
-        public ActionResult UserPage()
+        [Authorize]
+        public ActionResult UserPage(int id)
         {
-            //if (User.Identity.IsAuthenticated)
-            //{
-            //    var current_username = User.Identity.Name;
-            //    var current_user = db.Users.Where(x => x.USERNAME == current_username).FirstOrDefault();
-            //    var user = db.Users.Find(id);
-
-            //}
+            if (User.Identity.IsAuthenticated)
+            {
+                var current_username = User.Identity.Name;
+                var current_user = db.Users.Where(x => x.USERNAME == current_username).FirstOrDefault();
+                var user = db.Users.Find(id);
+                UserViewModel userViewModel = new UserViewModel();
+                userViewModel.FavoriteAuthors = db.FavoriteAuthors.Include(u => u.USER).Include(a => a.AUTHOR).Where(x => x.USER.USER_ID == user.USER_ID).ToList();
+                userViewModel.ReadBooksList = db.ReadBooksLists.Include(b => b.BOOK).Include(u => u.USER).Where(x => x.USER.USER_ID == user.USER_ID).ToList();
+                userViewModel.WantReadBooksList = db.WantReadBooksLists.Include(b => b.BOOK).Include(u => u.USER).Where(x => x.USER.USER_ID == user.USER_ID).ToList();
+                return View(userViewModel);
+            }
             return View();
         }
         //[Authorize]
---
+++ dBook/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using dBook.Models;
+namespace dBook.ViewModels
+{
+    public class UserViewModel
+    {
+        public List<WantReadBooksList> WantReadBooksList{ get; set; }
+
+        public List<ReadBooksList> ReadBooksList { get; set; }
+        public List<FavoriteAuthors> FavoriteAuthors { get; set; }
+    }
+}
