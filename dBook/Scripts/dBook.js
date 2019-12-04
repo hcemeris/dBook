6358d84ca2b3816d8d1325a1ca42b96b554a2a85
hcemeris <56699099+hcemeris@users.noreply.github.com>
2019-12-04 23:58:08 +0300

    MyPage sayfası oluşturuldu.
    UserPage ve HomePage sayafalarında düzenlemeler yapıldı.

---
+++ dBook/Scripts/dBook.js
@@ -0,0 +1,16 @@
+﻿
+    $("#btn1").click(function () {
+        $("#collapseExample2").hide();
+    $("#collapseExample3").hide();
+    $("#collapseExample1").show();
+});
+                $("#btn2").click(function () {
+        $("#collapseExample1").hide();
+    $("#collapseExample3").hide();
+    $("#collapseExample2").show();
+});
+                $("#btn3").click(function () {
+        $("#collapseExample1").hide();
+    $("#collapseExample2").hide();
+    $("#collapseExample3").show();
+ });
